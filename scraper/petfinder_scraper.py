2e7de3ccf32f14b1761bb7fc601dd99d7fd0c285
foontinz <foontinz@protonmail.com>
2022-08-07 14:45:18 +0300

    commit petfinder - try except statements

--- scraper/petfinder_scraper.py
+++ scraper/petfinder_scraper.py
@@ -253,6 +253,6 @@ class Scraper:
             return self.soup.find("a", {"class": "card_org-logo"})["href"].strip()
 
 
-print(Scraper(
+pprint(Scraper(
     "https://www.petfinder.com/member/us/ak/fairbanks/homeward-bound-pet-rescue-and-referral-ak29/",
     'shelter').object.unit)
